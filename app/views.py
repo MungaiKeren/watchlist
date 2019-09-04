3fdc93ffc70ebc63531fabfe1c6c353b3b530f65
MungaiKeren <wambukeren@gmail.com>
2019-09-04 16:34:32 +0300

    add a navbar and download flaskbootstrap

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 # views
 @app.route('/')
@@ -7,9 +8,15 @@ def index():
     '''
     view root function that returns the index page and gets its data
     '''
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    upcoming_movie = get_movies('upcoming')
+    print(upcoming_movie)
+    now_showing_movie = get_movies('now_playing')
+    print(now_showing_movie)
     title = 'Home - welcome to the best movie review website'
-    message = 'Hello World'
-    return render_template('index.html',title = title, message = message)
+    return render_template('index.html',title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
