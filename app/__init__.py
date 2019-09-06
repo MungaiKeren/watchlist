7fec369b5645eb954471764aa6fd792fd5205f7d
MungaiKeren <wambukeren@gmail.com>
2019-09-06 11:41:25 +0300

    call the configure requests function from the request.py

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,10 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    # setting configuration
+    from .request import configure_requests
+    configure_requests(app)
+
     # will add views and forms
 
     return app
